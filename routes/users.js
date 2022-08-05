4ff33fd26dbc1f4c7548cd28256ed221e29a8d52
md_mumtaj_alam <mmumtajalam987@gmail.com>
2022-08-05 21:28:20 +0530

    update

--- routes/users.js
+++ routes/users.js
@@ -16,4 +16,42 @@ router.get("/allusers", async (req, res) => {
   }
 });
 
+//http://localhost:4000/users/searchuser/001
+router.get("/searchuser/:userid", async (req, res) => {
+  try {
+    const tempId = req.params.userid;
+    const response = await db
+      .promise()
+      .query(`SELECT * FROM users WHERE userid = '${tempId}'`);
+    res.status(201).json(response[0]);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+//http://localhost:4000/users/finduser?id/001
+router.get("/finduser/", async (req, res) => {
+  try {
+    const tempId = req.query.id;
+    const response = await db
+      .promise()
+      .query(`SELECT * FROM users WHERE userid = '${tempId}'`);
+    res.status(201).json(response[0]);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+//http://localhost:4000/users/removeuser/01
+router.get("/removeuser/:id", async (req, res) => {
+  try {
+    const tempId = req.params.id;
+    const response = await db.promise().query(`DELETE FROM users WHERE
+      userid = '${tempId}'`);
+    res.status(201).json(response[0]);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
 module.exports = router;
